2d267176f74b429f96ab810c524b73f05af2e440
Hithaishi AL <hithaishial2007@gmail.com>
2026-03-19 22:01:44 +0530

    made changes in AccountAndTransactionService class

--- Banking System/src/com/thinkworks/banking/service/AccountAndTransactionService.java
+++ Banking System/src/com/thinkworks/banking/service/AccountAndTransactionService.java
@@ -13,32 +13,41 @@ public class AccountAndTransactionService {
     }
 
     public double calculateFinalBalance(Account account, Transaction transaction) {
+        double balanceAfterTransaction = account.getBalance();
 
-        double balanceAfterTransaction = 0;
-        if (transaction.getTransactionType() == "debit") {
+        if (transaction.getTransactionType().equals("debit")
+                && transaction.getSourceAccount().equals(account)) {
 
             System.out.println("Transaction ID: " + transaction.getTransactionId());
             System.out.println("Transaction type is debit");
-            System.out.println("Sender's details: " + account.getHolderName() + ", " + account.getBalance());
-            System.out.println("Receiver's details: " + account.getHolderName() + ", " + account.getBalance());
+            System.out.println("Sender's details: " + transaction.getSourceAccount().getHolderName()
+                    + ", Balance: " + transaction.getSourceAccount().getBalance());
+            System.out.println("Receiver's details: " + transaction.getDestinationAccount().getHolderName()
+                    + ", Balance: " + transaction.getDestinationAccount().getBalance());
             System.out.println("Transaction amount is: " + transaction.getAmount());
-            balanceAfterTransaction = balanceAfterTransaction - (transaction.getAmount());
-            System.out.println("Balance amount after debited: " + balanceAfterTransaction);
+
+            balanceAfterTransaction -= transaction.getAmount();
+            account.setBalance(balanceAfterTransaction);
+            System.out.println("Balance after debit: " + balanceAfterTransaction);
         }
-        if (transaction.getTransactionType() == "credit") {
+
+        if (transaction.getTransactionType().equals("credit")
+                && transaction.getDestinationAccount().equals(account)) {
 
             System.out.println("Transaction ID: " + transaction.getTransactionId());
             System.out.println("Transaction type is credit");
-            System.out.println("Sender's details: " + account.getHolderName() + ",  " + account.getBalance());
-            System.out.println("Receiver's details: " + account.getHolderName() + ",  " + account.getBalance());
+            System.out.println("Sender's details: " + transaction.getSourceAccount().getHolderName()
+                    + ", Balance: " + transaction.getSourceAccount().getBalance());
+            System.out.println("Receiver's details: " + transaction.getDestinationAccount().getHolderName()
+                    + ", Balance: " + transaction.getDestinationAccount().getBalance());
             System.out.println("Transaction amount is: " + transaction.getAmount());
-            balanceAfterTransaction = balanceAfterTransaction + transaction.getAmount();
-            System.out.println("Balance amount after credited: " + balanceAfterTransaction);
 
+            balanceAfterTransaction += transaction.getAmount();
+            account.setBalance(balanceAfterTransaction);
+            System.out.println("Balance after credit: " + balanceAfterTransaction);
         }
+
         return balanceAfterTransaction;
-
-
     }
 
 
